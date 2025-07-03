6a13ff83d4f1a92ecbc09374ebf9cee0a9e068b5
Vortex-oo <debjit.github@gmail.com>
2025-07-03 21:08:48 +0530

    delete route done

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,38 @@
+import User from "@/models/user.model";
+import connectDB from "../../../../../lib/db";
+import { NextResponse } from "next/server";
+
+
+interface DeleteMessageParams {
+    messageId: string;
+}
+
+export async function POST(req: Request, params: DeleteMessageParams): Promise<NextResponse> {
+    await connectDB();
+
+    const { messageId } = params;
+
+    try {
+        const result = await User.findByIdAndUpdate(
+            { messageId: messageId },
+            { $pull: { messages: { _id: messageId } } }
+        )
+
+        if (result.modifiedCount === 0) {
+            return NextResponse.json(
+                { success: false, message: "Message not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(
+            { success: true, message: "Message deleted successfully" },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { success: false, message: "Failed to delete message", error },
+            { status: 500 }
+        );
+    }
+}
