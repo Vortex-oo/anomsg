a93043b31b1834ffe9cb9229323e260991f8b6e2
Vortex-oo <debjit.github@gmail.com>
2025-07-03 10:00:46 +0530

    session fixed

--- auth.ts
+++ auth.ts
@@ -82,7 +82,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     session: {
         strategy: "jwt",
-        maxAge: 30 * 24 * 60 * 60, // 30 days
+        maxAge:60 * 60, // 3600 seconds (1 hour)
+        updateAge:15 * 60 // 15 minutes
     },
     secret: process.env.AUTH_SECRET,
     debug: process.env.NODE_ENV === "development",
