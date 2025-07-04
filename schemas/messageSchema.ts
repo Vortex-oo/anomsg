3433dd2be99bf2935c0b7c9f39d092b79fcc2905
Vortex-oo <debjit.github@gmail.com>
2025-07-04 12:19:31 +0530

    correct the message schema

--- schemas/messageSchema.ts
+++ schemas/messageSchema.ts
@@ -3,6 +3,6 @@ import {z} from 'zod';
 export const messageSchema = z.object({
     content: z.
             string()
-            .min(3, { message: 'Sender must be at least 3 characters long' })
-            .max(500, { message: 'Sender must be at most 500 characters long' }),
+            .min(3, { message: 'message must be at least 3 characters long' })
+            .max(500, { message: 'message must be at most 500 characters long' }),
 })
