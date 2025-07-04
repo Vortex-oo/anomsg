4eb1e7a7ef029ff3963845c2950f77c8cdda63fa
Vortex-oo <debjit.github@gmail.com>
2025-07-04 23:04:00 +0530

    fixed the get messages api

--- src/app/api/getmessages/route.ts
+++ src/app/api/getmessages/route.ts
@@ -4,63 +4,48 @@ import connectDB from "../../../../lib/db";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 
-
 export async function GET(req: Request) {
+    await connectDB();
 
-    //DB Connection
-
-    await connectDB()
-
-    const session = await auth()
-    const user = session?.user
+    const session = await auth();
+    const user = session?.user;
 
     if (!session || !user) {
-
-        return NextResponse.json({
-            message: "Not authenticated",
-            success: false
-        },
-            { status: 401 })
+        return NextResponse.json(
+            { message: "Not authenticated", success: false },
+            { status: 401 }
+        );
     }
 
     try {
         const userId = user.id;
 
         const userResult = await User.aggregate([
-            { $match: { id: userId } },
+            { $match: { _id: new mongoose.Types.ObjectId(userId) } },
             { $unwind: "$messages" },
-            { $sort: { "message.createdAt": -1 } },
-            { $group:{_id:"$_id", messages:{$push:"$messages"}}}
-        ])
+            { $sort: { "messages.createdAt": -1 } },
+            { $group: { _id: "$_id", messages: { $push: "$messages" } } }
+        ]);
 
-        if (!userResult) {
-            return NextResponse.json({
-                message: "User Not Found",
-                success: false
-            },
-                { status: 400 })
+        if (!userResult || userResult.length === 0) {
+            return NextResponse.json(
+                { message: "No Message Found", success: false },
+                { status: 404 }
+            );
         }
 
-        if ( userResult.length<=0) {
-            return NextResponse.json({
-                message:"No Message Found",
-                success:false
-            },{
-                status:400
-            })
-        }
-
-        return NextResponse.json({
-            messages:userResult[0].messages,
-            success:true
-        },{status:200})
-
+        return NextResponse.json(
+            { messages: userResult[0].messages, success: true },
+            { status: 200 }
+        );
     } catch (error) {
-        return NextResponse.json({
-            message: "Error in getting messages",
-            Errors:error,
-            success: false
-        },
-            { status: 401 })
+        return NextResponse.json(
+            {
+                message: "Error in getting messages",
+                error: (error as Error).message,
+                success: false,
+            },
+            { status: 500 }
+        );
     }
 }
